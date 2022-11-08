e45dc721d811f5655174fa926f8762690f4f57c2
GayashaBudddhi <it19061580@my.sliit.lk>
2022-11-08 23:33:33 +0530

    upload added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./src/Routes/userRoutes");
 const cors = require('cors');
+const fileRoute = require('./src/Routes/fileRoutes');
 const dotenv = require('dotenv');
 const app = express();
 
@@ -12,7 +13,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 mongoose
   .connect(
     MONGODB_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
   .then((_) => console.log("Connected to DB"))
   .catch((err) => console.error("error", err));
@@ -22,5 +23,6 @@ mongoose
 app.use(cors())
 app.use(express.json());
 app.use("/auth", userRoutes);
+app.use("/file", fileRoute);
 
 app.listen(8000, () => console.log("Running on port 8000"));
